38e45122e4705af1caa789dc813fd1f0cc413f85
Cpraveen2002 <praveenkumar.ch20@iiits.in>
2024-04-14 01:17:42 +0530

    Hi

---
+++ dropdown.js
@@ -0,0 +1,27 @@
+$(".chief").on("click",function(){
+    $(".first").slideToggle();
+})
+$(".Co-Chair").on("click",function(){
+    $(".seventh").slideToggle();
+})
+$(".Chairperson").on("click",function(){
+    $(".eight").slideToggle();
+})
+$(".Technical").on("click",function(){
+    $(".second").slideToggle();
+})
+$(".Workshop").on("click",function(){
+    $(".third").slideToggle();
+})
+$(".Pubilicity").on("click",function(){
+    $(".fourth").slideToggle();
+})
+$(".Finance").on("click",function(){
+    $(".fifth").slideToggle();
+})
+$(".Registration").on("click",function(){
+    $(".six").slideToggle();
+})
+$(".Student-Coordinators").on("click",function(){
+    $(".Nine").slideToggle();
+})
